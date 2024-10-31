cd686e83490aee5637678abd518ac4560e63ca56
mattKooGIT <matthewkoo366@gmail.com>
2024-10-30 23:46:19 -0400

    update

--- survey.js
+++ survey.js
@@ -1,31 +1,25 @@
 function retrieve(){
-
-    //retrieve information
-    var firstname = document.getElementById('firstname').value;
-    var lastname = document.getElementById('lastname').value;
-    var email = document.getElementById('email').value;
-    var reason = document.querySelector('input[list="browsers"]').value;
-    var school = document.getElementById('school').value;
-    var otherSchool = document.getElementById('other').value;
-    var discoveryMethod = document.querySelector('input[name="discovery"]:checked')?.value;
-    var suggestions = document.querySelector('textarea[name="message"]').value;
-    console.log("awef");
-    //create an object to hold all of the survey data
-    var surveyData = {
-        firstname: firstname,
-        lastname: lastname,
-        email: email,
-        reason: reason,
-        school: school,
-        otherSchool: otherSchool,
-        discoveryMethod: discoveryMethod,
-        sectionsInterested: sections,
-        suggestions: suggestions
-    };
-
-    // store the data in local storage
-    localStorage.setItem('surveyData', JSON.stringify(surveyData));
-
+    //retrieve data from each element
+    var firstname = document.getElementById("firstname");
+    localStorage.setItem("firstname", firstname);
+    var lastname = document.getElementById("lastname");
+    localStorage.setItem("lastname", lastname);
+    var email = document.getElementById("email");
+    localStorage.setItem("email", email);
+    var reasons = document.querySelector('input[list="browsers"]');
+    localStorage.setItem("reasons", reasons);
+    var school = document.getElementById("school");
+    localStorage.setItem("school", school);
+    var other = document.getElementById("other");
+    localStorage.setItem("other", other);
+    var discoveryMethod = document.querySelector('input[name="discovery"]:checked');
+    localStorage.setItem("discovery", discoveryMethod);
+    var suggestions = document.querySelector('textarea[name="message"]').trim();
+    localStorage.setItem("suggestions", suggestions);
+    
+    //check multiselect box
+    var helpful = Array.from(document.querySelectorAll('input[name="interest"]:checked')).map(checkbox => checkbox.id);
+    localStorage.setItem("helpful", helpful);
     alert('Survey data saved successfully!');
 }
 
